7d280a5f310e13c9f071b5974a90fdcfae6cc7bb
Greg Heartsfield <scsibug@imap.cc>
2010-08-18 21:43:06 -0500

    Feed reading from CLI args.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.2.0-RC3"]
                  [org.clojure/clojure-contrib "1.2.0-RC3"]
 		 [lein-run "1.0.0-SNAPSHOT"]
-		 [jdom/jdom "1.1"]
+		 [org.jdom/jdom "1.1"]
 		 [rome/rome "1.0"]]
   :main feedparser-clj.core)
 
