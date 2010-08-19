5881ee670664887b1bfa92d5c9a7fb802dcd26be
Greg Heartsfield <scsibug@imap.cc>
2010-08-18 20:16:07 -0500

    Added Lein Run plugin.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0-RC3"]
                  [org.clojure/clojure-contrib "1.2.0-RC3"]
+		 [lein-run "1.0.0-SNAPSHOT"]
 		 [rome/rome "1.0"]]
   :main feedparser-clj.core)
 
