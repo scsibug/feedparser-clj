e724e89f8989ee65b4684534f6130fcf6fa4f0a9
Greg Heartsfield <scsibug@imap.cc>
2010-08-18 19:30:21 -0500

    Added dependency on Project Rome for RSS parsing. Maven didn't find the Rome jar, so had to install manually.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject feedparser-clj "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0-RC3"]
-                 [org.clojure/clojure-contrib "1.2.0-RC3"]])
+                 [org.clojure/clojure-contrib "1.2.0-RC3"]
+		 [rome/rome "1.0"]])
