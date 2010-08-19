a62e30a7aab78716d8446cbd48757e7cba388f21
Greg Heartsfield <scsibug@imap.cc>
2010-08-19 00:00:24 -0500

    Removed unnecessary printlns.

--- src/feedparser_clj/core.clj
+++ src/feedparser_clj/core.clj
@@ -6,11 +6,8 @@
 	'(com.sun.syndication.feed.synd SyndFeed))
 
 (defn -main [& args]
-  (println (cons "Feed URL: " args))
   (let [feedinput (new SyndFeedInput)
 	xmlreader (new XmlReader (new URL (first args)))]
-    (println "About to call build")
-    (.build feedinput xmlreader)
+    (println (.build feedinput xmlreader))
     )
-    (println "Finished reading/printing feed")
   )
