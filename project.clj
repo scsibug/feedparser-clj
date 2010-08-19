2ff1a27d9099ee14e52e02a7c3e713ad2fe10558
Greg Heartsfield <scsibug@imap.cc>
2010-08-18 20:04:39 -0500

    Added imports for Rome, and main class definition.

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0-RC3"]
                  [org.clojure/clojure-contrib "1.2.0-RC3"]
-		 [rome/rome "1.0"]])
+		 [rome/rome "1.0"]]
+  :main feedparser-clj.core)
+
--- src/feedparser_clj/core.clj
+++ src/feedparser_clj/core.clj
@@ -1,1 +1,11 @@
 (ns feedparser-clj.core)
+
+(import '(com.sun.syndication.io.SyndFeedInput)
+	'(java.net.URL)
+	'(java.io.InputStreamReader)
+	'(com.sun.syndication.feed.synd.SyndFeed)
+	'(com.sun.syndication.io.SyndFeedInput)
+	'(com.sun.syndication.io.XmlReader))
+
+(defn -main [& args]
+  (println "Hello world!"))
