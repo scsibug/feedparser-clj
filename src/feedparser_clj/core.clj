7e8669fde63f8b94342b2b4ee9a165c904c28037
Greg Heartsfield <scsibug@imap.cc>
2010-08-19 23:49:53 -0500

    New function for retrieving feeds.

--- src/feedparser_clj/core.clj
+++ src/feedparser_clj/core.clj
@@ -6,10 +6,16 @@
 	'(java.io InputStreamReader)
 	'(com.sun.syndication.feed.synd SyndFeed))
 
-(defn -main [feedurl]
-  (println (str "Using feed: " feedurl))
+(defn -main "Show basic information for a feed, given a URL"
+  [feedurl]
+  (println "Using feed:" feedurl)
+  (def myfeed (getFeed feedurl))
+  (println "Found" (count (.getEntries myfeed)) "entries")
+  )
+
+(defn getFeed "Get a SyndFeed object from a URL"
+  [feedurl]
   (let [feedinput (new SyndFeedInput)
-	xmlreader (new XmlReader (new URL feedurl))]
-    (println (.build feedinput xmlreader))
-    )
+        xmlreader (new XmlReader (new URL feedurl))]
+    (.build feedinput xmlreader))
   )
