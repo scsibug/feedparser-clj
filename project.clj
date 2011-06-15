c91d8140c2be49dd613656d1eef66fc08b76cb8b
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-06-15 19:44:31 +0200

    New version with new dependencies.
    No snapshot deps.

--- project.clj
+++ project.clj
@@ -1,9 +1,6 @@
-(defproject feedparser-clj "0.1.0-SNAPSHOT"
+(defproject feedparser-clj "0.2"
   :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
-  :dependencies [[org.clojure/clojure "1.2.0-RC3"]
-                 [org.clojure/clojure-contrib "1.2.0-RC3"]
-		 [lein-run "1.0.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
 		 [org.jdom/jdom "1.1"]
-		 [rome/rome "1.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
-  :main feedparser-clj.core)
+		 [net.java.dev.rome/rome "1.0.0"]])
