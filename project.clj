f9b870db7b89bda9346b066df03f87d2a6ab669c
Pierre-Yves Ritschard <pyr@spootnik.org>
2011-11-23 11:36:15 +0100

    update for clojure-1.3

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.vgeshel/feedparser-clj "0.3"
   :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
 		 [org.jdom/jdom "1.1"]
-		 [net.java.dev.rome/rome "1.0.0"]])
+		 [net.java.dev.rome/rome "1.0.0"]]
+  :main feedparser-clj.core)
