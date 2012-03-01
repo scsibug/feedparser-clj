7ddca5bf368530e7a7b117a085ccb66b534b8020
Greg Heartsfield <scsibug@imap.cc>
2012-02-29 20:26:15 -0600

    Change org again for clojars.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.smallrivers/feedparser-clj "0.3"
+(defproject org.clojars.scsibug/feedparser-clj "0.3"
   :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [org.jdom/jdom "1.1"]
