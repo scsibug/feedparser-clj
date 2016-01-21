12c56f393c2a6b3bcd65f7ff7fb4e06f8a54fff4
Reynald Borer <reynald.borer@paper.li>
2016-01-21 17:40:00 +0100

    Bump to clojure 1.8.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.clojars.smallrivers/feedparser-clj "0.5.0"
   :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.jdom/jdom "2.0.2"]
                  [com.rometools/rome "1.5.1"]])
