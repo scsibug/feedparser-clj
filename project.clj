129c73fd02541c7a90847feeed44e5e9a085dcb6
Luke Skibinski <lsh@ccxx.cx>
2017-09-09 13:04:01 +0930

    dependency bump: jdom to jdom2 2.0.6, rome to 1.7.4

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.clojars.scsibug/feedparser-clj "0.5.1"
   :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
   :dependencies [[org.clojure/clojure "1.8.0"]
-		 [org.jdom/jdom "2.0.2"]
-		 [com.rometools/rome "1.7.0"]])
+		 [org.jdom/jdom2 "2.0.6"]
+		 [com.rometools/rome "1.7.4"]])
