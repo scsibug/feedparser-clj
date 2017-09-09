9b9364f2c5004c581d5d68cb1192858d441d8939
Luke Skibinski <lsh@ccxx.cx>
2017-09-09 11:28:42 +0930

    fixed typo in rome dependency I likely messed something up in the merge

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
   :dependencies [[org.clojure/clojure "1.8.0"]
 		 [org.jdom/jdom "2.0.2"]
-		 [net.java.dev.rome/rome "1.7.0"]])
+		 [com.rometools/rome "1.7.0"]])
